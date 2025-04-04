e9e732faf653e1181cfa422ec82814f4a0e3b259
Lepros311 <andy.strickland@gmail.com>
2025-04-03 20:57:10 -0400

    errors

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
 ﻿using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Protocols;
 using System.Configuration;
+using Microsoft.Extensions.Configuration;
 
 Console.Title = "Flashcards";
 
+string currentDirectory = Directory.GetCurrentDirectory();
 
+string projectDirectory = Path.Combine(currentDirectory, @"..\..\..");
+string appSettingsPath = Path.Combine(projectDirectory, "Properties");
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile($"{appSettingsPath}\\appsettings.json", optional: true, reloadOnChange: true)
+    .Build();
+
+string connectionString = configuration.GetConnectionString("connection");
