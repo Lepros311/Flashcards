7c7c2efee8b94180193007c86950c24366e1837d
Lepros311 <andy.strickland@gmail.com>
2025-04-29 12:33:20 -0400

    Fix Codacy issues

--- obj/Debug/net8.0/Flashcards.AssemblyInfo.cs
+++ obj/Debug/net8.0/Flashcards.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flashcards")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c69506c08e61c8d8957ae705a82ae32349e432f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84c4297aa120fd42597425a894c35c312e08d448")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flashcards")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flashcards")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
